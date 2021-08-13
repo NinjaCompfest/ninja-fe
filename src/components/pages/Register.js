6b56219434817eb71fc6d8d616527e6c07d33a3a
Maryanto Cin <firevolt719@gmail.com>
2021-08-13 17:15:08 +0700

    Change class to className and few styling changes

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -10,7 +10,8 @@ const Register = () => {
       <form class="bg-white shadow-md px-8 pt-6 pb-8 mb-4 h-3/4 rounded-l-lg">
         <h1 className="text-2xl font-bold">Let's get started.</h1>
         <span className="block text-gray-700 text-sm font-bold mb-6">
-          Already have an account? Log in now
+          Already have an account? <span className="text-blue-700">Log in</span>{" "}
+          now
         </span>
         <div class="mb-4">
           <Input label="Full name" type="text" placeholder="Enter full name" />
