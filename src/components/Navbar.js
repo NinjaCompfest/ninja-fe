db9b1aae203264373ef04c05e53078707c38b68b
Maryanto Cin <firevolt719@gmail.com>
2021-08-15 15:33:15 +0700

    Fixed href errors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,33 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
 const Navbar = (props) => {
-    return (
-        <div className='bg-white bar'>
-            <div className='max-w-6xl mx-auto'>
-                <div className='flex justify-between'>
+  return (
+    <div className="bg-white bar">
+      <div className="max-w-6xl mx-auto">
+        <div className="flex justify-between">
+          {/* logo */}
+          <div>
+            <a
+              href="/"
+              className="logo flex items-center py-3 px-3 text-red-600"
+            >
+              NINJA
+            </a>
+          </div>
 
-                    {/* logo */}
-                    <div>
-                        <a href="#" className='logo flex items-center py-3 px-3 text-red-600'>NINJA</a>
-                    </div>
-
-                    {/* secondary nav */}
-                    <div className='flex items-center space-x-1'>
-                        <a href='{props.link}' className='py-1 px-7 border border-gray-400 rounded hover:bg-black hover:text-white transition duration-300'>{props.status}</a>
-                    </div>
-                </div>
-            </div>
+          {/* secondary nav */}
+          <div className="flex items-center space-x-1">
+            <a
+              href="{props.link}"
+              className="py-1 px-7 border border-gray-400 rounded hover:bg-black hover:text-white transition duration-300"
+            >
+              {props.status}
+            </a>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
