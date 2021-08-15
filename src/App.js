2b3f6866c12152e5eeaf208c8d93435564b5a836
al-ayubi2020 <al.ayubi2020@gmail.com>
2021-08-15 20:37:32 +0700

    Donation, withdraw page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import CrateProgram from "./CrateProgram";
 import DonatePage from "./DonatePage";
+import ProgramConfirmationDetail from "./ProgramConfirmationDetail";
 import ProgramDetail from "./ProgramDetail";
+import Withdraw from "./Withdraw";
 
 function App() {
   return (
     <div className="bg-gray-300 min-h-screen">
-      <ProgramDetail />
+      <CrateProgram />
     </div>
   );
 }
---
+++ src/CrateProgram.jsx
@@ -0,0 +1,73 @@
+import { Component } from "react";
+import Navbar from "./components/Navbar";
+import "./components/TopUpPage.css";
+import "./components/ProfileBox.css"
+
+class CrateProgram extends Component {
+  state = {
+    valueTitle: '',
+    valueDescription: '',
+  };
+
+  titleInput(event) {
+    const inputValue = event.target.value;
+    this.setState({ valueTitle: inputValue });
+  }
+  
+  descriptionInput(event) {
+    const inputValue = event.target.value;
+    this.setState({ valueDescription: inputValue });
+  }
+
+  render() {
+    return (
+      <div className="bg-gray-300 min-h-screen">
+        <Navbar status={this.state.status} link={this.state.link} />
+        <div className="flex items-center justify-center min-h-screen">
+          <div className="container max-w-xl">
+            <div className="grid grid-cols-3 gap-4 self-center p-10 max-h-full">
+              <div className="col-span-3 text-center text-2xl font-bold">
+                Crate Donation Program
+              </div>
+              <div className="col-span-3 px-10 mt-5">Program Name</div>
+              <div className="col-span-3 px-10">
+                <input
+                  className="min-w-full bg-gray-300 input-box px-10"
+                  value={this.state.valueTitle}
+                  onChange={(event) => {
+                    this.titleInput(event);
+                  }}
+                ></input>
+              </div>
+              <div className="col-span-3 px-10">Program Description</div>
+              <div className="col-span-3 px-10">
+                <textarea
+                  className="min-w-full bg-gray-300 textarea-box px-10 overflow-auto"
+                  value={this.state.valueDescription}
+                  onChange={(event) => {
+                    this.descriptionInput(event);
+                  }}
+                ></textarea>
+              </div>
+              <div className="col-span-3 px-10 mt-5">
+                <div className="grid grid-cols-2 gap-8 self-center max-h-full">
+                  <a
+                    href="/"
+                    className="border border-black text-xl rounded-xl py-1 text-center bg-black text-white hover:bg-white hover:text-black transition duration-300"
+                  >
+                    Cancel
+                  </a>
+                  <button className="border border-black text-xl rounded-xl py-1 text-center bg-black text-white hover:bg-white hover:text-black transition duration-300">
+                    Make New
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CrateProgram;
