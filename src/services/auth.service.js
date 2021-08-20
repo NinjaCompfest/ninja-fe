61dc391a7f6949c1901461e1e6c1a04837703dcc
Maryanto Cin <firevolt719@gmail.com>
2021-08-20 17:54:09 +0700

    Hard coded base url

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import config from "../config";
 
-axios.defaults.baseURL = process.env.REACT_APP_BACKEND_BASE_URL;
+axios.defaults.baseURL = "http://ninja-be.herokuapp.com";
 
 export const register = async (full_name, username, password, type) =>
   axios.post(config.register, {
